07731d9ddebc37421be96e1ee904e8502bbe0319
allanrothenburg <allantrickrothenburg@gmail.com>
2022-07-06 13:08:16 -0300

    Adicionando chamadas de API em store VueX

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import Vue from 'vue'
 import Vuex from 'vuex'
 
@@ -5,12 +6,75 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    users: [],
   },
   getters: {
+    users: state => state.users,
   },
   mutations: {
+    SET_USERS(state, users) {
+      state.users = users;
+    }
   },
   actions: {
+    getUsers({ commit }) {
+      return new Promise((resolve, reject) => {
+        axios
+          .get('https://reqres.in/api/users')
+          .then(response => {
+            commit('SET_USERS', response.data.data);
+            resolve(response.data.data)
+          })
+          .catch(error => {
+            reject(error);
+          });
+      });
+    },
+    createUser({ commit, dispatch }, user) {
+      return new Promise((resolve, reject) => {
+        axios
+          .post('https://reqres.in/api/users', user)
+          .then(response => {
+            resolve(response.data.data);
+          })
+          .catch(error => {
+            reject(error);
+          })
+          .finally(() => {
+            dispatch('getUsers');
+          })
+      });
+    },
+    updateUser({ commit, dispatch }, user) {
+      return new Promise((resolve, reject) => {
+        axios
+          .put(`https://reqres.in/api/users/${user.id}`, user)
+          .then(response => {
+            resolve(response.data.data);
+          })
+          .catch(error => {
+            reject(error);
+          })
+          .finally(() => {
+            dispatch('getUsers');
+          })
+      });
+    },
+    deleteUser({ commit, dispatch }, user) {
+      return new Promise((resolve, reject) => {
+        axios
+          .delete(`https://reqres.in/api/users/${user.id}`)
+          .then(response => {
+            resolve(response.data.data);
+          })
+          .catch(error => {
+            reject(error);
+          })
+          .finally(() => {
+            dispatch('getUsers');
+          })
+      });
+    }
   },
   modules: {
   }
---
+++ src/views/ListUsers.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    <h1 @click="getUsers()">getUsers</h1>
+    <h1 @click="createUser()">createUser</h1>
+    <h1 @click="updateUser()">updateUser</h1>
+    <h1 @click="deleteUser()">deleteUser</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.$store.dispatch("getUsers");
+  },
+  methods: {
+    getUsers() {
+      this.$store.dispatch("getUsers");
+    },
+    createUser() {
+      this.$store.dispatch("createUser", this.$store.getters['users'][0]);
+    },
+    updateUser() {
+      this.$store.dispatch("updateUser", this.$store.getters['users'][0]);
+    },
+    deleteUser() {
+      this.$store.dispatch("deleteUser", this.$store.getters['users'][0]);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
